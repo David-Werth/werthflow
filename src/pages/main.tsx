16fcda50fbeaca70c9ae1adbfd2ceb68cf96c7f9
David-Werth <75678655+David-Werth@users.noreply.github.com>
2023-11-02 06:54:45 +0100

    fix: afterSignOutUrl changed

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,20 +1,24 @@
 import TodoCard from '@/components/main/todo-card';
+import { cards } from '@/lib/mock/card-data';
 import { UserButton } from '@clerk/clerk-react';
 
 export default function Main() {
 	return (
 		<div className="flex items-center justify-center w-full h-full">
 			<div className="fixed right-5 top-5">
-				<UserButton />
+				<UserButton afterSignOutUrl="http://localhost:5173/" />
 			</div>
 			<div className="grid max-w-3xl grid-cols-4">
 				<div>
-					test
-					<TodoCard />
-					<TodoCard />
+					To-do
+					{cards.map((card) => {
+						return (
+							<TodoCard state={card.state} title={card.title} content={card.content} />
+						);
+					})}
 				</div>
-				<div>test</div>
-				<div>test</div>
+				<div>In Progress</div>
+				<div>Done</div>
 			</div>
 		</div>
 	);
