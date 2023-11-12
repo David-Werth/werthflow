f199c3e06fd1732002d8268b2f67a7b9f263fac3
David-Werth <75678655+David-Werth@users.noreply.github.com>
2023-11-12 09:26:37 +0100

    chore: Lifts ModeToggle to public, Clerk Theme

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ import PublicPage from './pages/public-page';
 import SignInPage from './pages/auth/signin-page';
 import SignUpPage from './pages/auth/signup-page';
 import ModeToggle from './components/shared/mode-toggle';
-import { ThemeProvider } from './components/providers/theme-provider';
+import { ThemeProvider, useTheme } from './components/providers/theme-provider';
 import { TaskContextWrapper } from './components/providers/task-provider';
+import { dark } from '@clerk/themes';
 
 if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
 	throw new Error('Missing Publishable Key');
@@ -21,9 +22,14 @@ const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 function ClerkProviderWithRoutes() {
 	const navigate = useNavigate();
+	const { theme } = useTheme();
 
 	return (
-		<ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+		<ClerkProvider
+			publishableKey={clerkPubKey}
+			navigate={(to) => navigate(to)}
+			appearance={theme === 'dark' ? { baseTheme: dark } : {}}
+		>
 			<Routes>
 				<Route path="/sign-in/*" element={<SignInPage />} />
 				<Route path="/sign-up/*" element={<SignUpPage />} />
@@ -36,13 +42,13 @@ function ClerkProviderWithRoutes() {
 								<div className="fixed right-5 top-5">
 									<UserButton afterSignOutUrl="/" />
 								</div>
-								<div className="fixed left-5 bottom-5">
-									<ModeToggle />
-								</div>
 							</SignedIn>
 							<SignedOut>
 								<PublicPage />
 							</SignedOut>
+							<div className="fixed left-5 bottom-5">
+								<ModeToggle />
+							</div>
 						</>
 					}
 				/>
