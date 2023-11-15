fd33c64ca49664887a4f8f70e03b1bad6b3cc1ad
David Werth <info@davidwerth.com>
2023-11-15 06:25:10 +0100

    feat: add loading state

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,7 +1,8 @@
 import Board from '@/components/main/board';
 import { TaskContext } from '@/components/providers/task-provider';
 import { useUser } from '@clerk/clerk-react';
-import { useContext, useEffect } from 'react';
+import { Cable, Loader2 } from 'lucide-react';
+import { useContext, useEffect, useState } from 'react';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -10,10 +11,14 @@ export default function Main() {
 
 	const { items, setItems } = useContext(TaskContext);
 
+	const [isLoading, setIsLoading] = useState(false);
+
+	const [isError, setIsError] = useState(false);
+
 	useEffect(() => {
 		if (user?.username) {
 			/*
-			 * Update or insert a user in the database
+			 * Create user after signin
 			 */
 			async function createUser(id: string, username: string, items: string) {
 				const res = await fetch(`${apiUrl}/user/${id}`, {
@@ -24,15 +29,16 @@ export default function Main() {
 					body: JSON.stringify({ name: username, tasks: items }),
 				});
 				if (res.ok) {
-					const { data } = await res.json();
-					console.log(data);
-				} else console.log('not nice');
+					setIsLoading(false);
+				} else setIsError(true);
 			}
 
 			/*
 			 * Function to get user data from the database
 			 */
 			async function getUser(id: string, username: string) {
+				setIsLoading(true);
+
 				const res = await fetch(`${apiUrl}/user/${id}`);
 
 				if (res.ok) {
@@ -41,6 +47,7 @@ export default function Main() {
 						createUser(id, username, JSON.stringify(items));
 					} else {
 						setItems(JSON.parse(data.tasks));
+						setIsLoading(false);
 					}
 				} else {
 					createUser(id, username, JSON.stringify(items));
@@ -62,9 +69,9 @@ export default function Main() {
 			});
 
 			if (res.ok) {
-				// const { data } = await res.json();
+				setIsError(false);
 			} else {
-				console.log('We have a problem', res.status);
+				setIsError(true);
 			}
 		}
 		if (user?.id) updateUserTasks(user.id, JSON.stringify(items));
@@ -72,7 +79,18 @@ export default function Main() {
 
 	return (
 		<>
-			<Board />
+			{isLoading ? (
+				<div className="flex flex-col items-center justify-center w-full h-full gap-5">
+					<Loader2 className="w-10 h-10 animate-spin" />
+					{isError && (
+						<p className="text-xl">
+							Please check your connection! <Cable className="inline-block" />
+						</p>
+					)}
+				</div>
+			) : (
+				<Board />
+			)}
 		</>
 	);
 }
